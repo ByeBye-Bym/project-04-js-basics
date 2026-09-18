8ab09ad273800654689970f9874e0bdeac254cdd
Ваше Имя <_email@example.com>
2026-09-18 11:23:07 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// ПЗ №4. Дополните код.
+const studentName = 'Ваше имя';
+const courseName = 'Основы веб-разработки';
+let completedTasks = 0;
+const totalTasks = 10;
+
+// TODO: вычислите remainingTasks и progress.
+// TODO: выведите результаты в console.log().
+// TODO: добавьте if ... else.
