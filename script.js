13be94ef4fef7bb464a16bccd9d97001c04e6911
ByeBye-Bym <ruckinvova75@gmail.com>
2026-09-22 02:12:16 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-// ПЗ №4. Дополните код.
-const studentName = 'Ваше имя';
+const studentName = 'Владимир';
 const courseName = 'Основы веб-разработки';
-let completedTasks = 0;
+
+let completedTasks = 10;
 const totalTasks = 10;
 
-// TODO: вычислите remainingTasks и progress.
-// TODO: выведите результаты в console.log().
-// TODO: добавьте if ... else.
+const remainingTasks = totalTasks - completedTasks;
+const progress = Math.round(completedTasks / totalTasks * 100);
+
+console.log(`Студент: ${studentName}`);
+console.log(`Курс: ${courseName}`);
+console.log(`Осталось заданий: ${remainingTasks}`);
+console.log(`Прогресс: ${progress}%`);
+
+if (completedTasks === totalTasks) {
+    console.log('Работа завершена');
+} else {
+    console.log('Продолжай работу');
+}
+
+if (progress >= 80) {
+    console.log('Финиш близко');
+}
+
+const hoursSpent = 30;
+const avgHoursPerTask = hoursSpent / completedTasks;
+console.log(`Среднее время на задание: ${avgHoursPerTask} ч.`);
